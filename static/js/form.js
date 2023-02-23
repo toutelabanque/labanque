a05d805f23f8fc555c3ace3a344fd018d789ca76
Xbox One <sondad2008@gmail.com>
2023-02-23 00:02:03 -0500

    Fixed password confirmation validity checker.
    Also:
    - Renamed home button in toolbar to La Banque.
    -Removed La Banque h1 from toolbar along with associated styling.
    - Removed redundant type="submit" from form submission buttons in Login and Sign Up forms.
    - Removed no longer relevant part of if  new dialog in Login form.
    - Changed placeholder to say Member ID instead of Account ID for id input in Login form.

--- static/js/form.js
+++ static/js/form.js
@@ -6,14 +6,17 @@ console.log(confirmPassword)
 
 if (password !== null && confirmPassword !== null) {
     console.log("not null")
-    confirmPassword.addEventListener('keypress', (_) => {
-        console.log("keypress")
+    confirmPassword.addEventListener('blur', (_) => {
         if (password.value != confirmPassword.value) {
             password.setCustomValidity("Passwords don't match.")
             confirmPassword.setCustomValidity("Passwords don't match.")
             password.setAttribute("title", "Passwords don't match.")
             confirmPassword.setAttribute("title", "Passwords don't match.")
-            console.log("validity set")
+        } else {
+            password.setCustomValidity("")
+            confirmPassword.setCustomValidity("")
+            password.setAttribute("title", "")
+            confirmPassword.setAttribute("title", "")
         }
     })
 }
